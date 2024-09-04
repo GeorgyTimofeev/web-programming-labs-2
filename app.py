f4d39b743ad1a0cf4970d4c62b26479cd31d974e
GeorgyTimofeev <timofeevga22@gmail.com>
2024-09-04 17:07:37 +0700

    Информация об авторе

--- app.py
+++ app.py
@@ -2,11 +2,28 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def start():
+@app.route('/web')
+def web():
     return '''<!doctype html>
         <html>
            <body>
                <h1>web-сервер на flask</h1>
-           </body> \
+                <a href="/author">Автор</a>
+           </body>
+        </html>'''
+
+@app.route('/author')
+def author():
+    name = "Тимофеев Георгий Алексеевич"
+    group = "ФБИ-22"
+    faculty = "ФБ"
+
+    return '''<!doctype html>
+        <html>
+            <body>
+                <p>Студент: ''' + name + '''</p>
+                <p>Группа: ''' + group + '''</p>
+                <p>Факультет: ''' + faculty + '''</p>
+                <a href="/web">На страницу "web"</a>
+            </body>
         </html>'''
