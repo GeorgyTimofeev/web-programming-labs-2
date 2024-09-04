b39dab0f1e7899bd260001701f91835cbaac0bbf
GeorgyTimofeev <timofeevga22@gmail.com>
2024-09-04 17:41:12 +0700

    Счетчик

--- app.py
+++ app.py
@@ -40,3 +40,18 @@ def oak():
     </body>
 </html>
 '''
+
+count = 0
+@app.route('/lab1/counter')
+def counter():
+    global count
+    count += 1
+    return '''
+<!doctype html>
+<html>
+    <body>
+        <h1>Счётчик</h1>
+        <p>Сколько раз вы заходили на эту страницу: ''' + str(count) + '''</p>
+    </body>
+</html>
+'''
