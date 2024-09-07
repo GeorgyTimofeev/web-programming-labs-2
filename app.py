743adf4dc81dea66d9057bc04a101a98a1ba3f14
GeorgyTimofeev <timofeevga22@gmail.com>
2024-09-07 12:01:02 +0700

    Меню первой лабораторной

--- app.py
+++ app.py
@@ -28,6 +28,30 @@ def index():
 </html>
 '''
 
+@app.route('/lab1')
+def lab1():
+    return'''
+<!doctype html>
+<html>
+    <head>
+        <tytle>Лабораторная 1</tytle>
+        <link rel="stylesheet" type="text/css" href="''' + url_for('static', filename='lab1.css') + '''">
+    </head>
+    <body>
+        <h1>Flask</h1>
+        <p>Flask — фреймворк для создания веб-приложений на языке программирования Python,
+        использующий набор инструментов Werkzeug, а также шаблонизатор Jinja2. Относится
+        к категории так называемых микрофреймворков — минималистичных каркасов веб-приложений,
+        сознательно предоставляющих лишь самые базовые возможности.</p>
+
+        <a href="/">Назад на главную</a></li>
+    </body>
+    <footer>
+        Тимофеев Георгий Алексеевич, ФБИ-22, 3 Курс, 2024 год.
+    </footer>
+</html>
+'''
+
 @app.route('/lab1/web')
 def web():
     return '''<!doctype html>
