02989c7ccf42d599b0447d8674fcdd205e8c0fbc
GeorgyTimofeev <timofeevga22@gmail.com>
2024-09-07 11:17:39 +0700

    Исправление адресов

--- app.py
+++ app.py
@@ -6,20 +6,23 @@ app = Flask(__name__)
 def not_found(err):
     return "Такой страницы не существует", 404
 
-@app.route('/web')
+@app.route('/lab1/web')
 def web():
     return '''<!doctype html>
         <html>
+           <head>
+                <link rel="stylesheet" type="text/css" href="''' + url_for('static', filename='lab1.css') + '''">
+            </head>
            <body>
                <h1>web-сервер на flask</h1>
-                <a href="/author">Автор</a>
+                <a href="/lab1/author">Автор</a>
            </body>
         </html>''', 200, {
             'X-Server': 'sample',
             'Content-Type': 'text/plain; charset=utf-8'
         }
 
-@app.route('/author')
+@app.route('/lab1/author')
 def author():
     name = "Тимофеев Георгий Алексеевич"
     group = "ФБИ-22"
@@ -27,11 +30,14 @@ def author():
 
     return '''<!doctype html>
         <html>
+            <head>
+                <link rel="stylesheet" type="text/css" href="''' + url_for('static', filename='lab1.css') + '''">
+            </head>
             <body>
                 <p>Студент: ''' + name + '''</p>
                 <p>Группа: ''' + group + '''</p>
                 <p>Факультет: ''' + faculty + '''</p>
-                <a href="/web">На страницу "web"</a>
+                <a href="/lab1/web">На страницу "web"</a>
             </body>
         </html>'''
 
@@ -60,6 +66,9 @@ def counter():
     return '''
 <!doctype html>
 <html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="''' + url_for('static', filename='lab1.css') + '''">
+    </head>
     <body>
         <h1>Счётчик</h1>
         <p>Сколько раз вы заходили на эту страницу: ''' + str(count) + '''</p>
@@ -74,15 +83,18 @@ def counter_cleaner():
     count = 0
     return redirect('/lab1/counter')
 
-@app.route('/info')
+@app.route('/lab1/info')
 def info():
-    return redirect('/author')
+    return redirect('/lab1/author')
 
 @app.route('/lab1/created')
 def created():
     return '''
 <!doctype html>
 <html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="''' + url_for('static', filename='lab1.css') + '''">
+    </head>
     <body>
         <h1>Создано успешно</h1>
         <div><i>что-то создано…</i></div>
