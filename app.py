6a88c2549af6841fe982c82f6641df084c9fdc72
GeorgyTimofeev <timofeevga22@gmail.com>
2024-09-24 21:50:29 +0700

    Имя студента

--- app.py
+++ app.py
@@ -468,4 +468,5 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name= "Тимофеев Георгий"
+    return render_template('example.html', name=name)
