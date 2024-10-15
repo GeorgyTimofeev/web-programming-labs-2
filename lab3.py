91681d7f770876987bcada761ca7111c31d52b72
GeorgyTimofeev <timofeevga22@gmail.com>
2024-10-15 22:41:58 +0700

    Форма

--- lab3.py
+++ lab3.py
@@ -22,3 +22,31 @@ def del_cookies():
     resp.delete_cookie('age')
     resp.delete_cookie('name_color')
     return resp
+
+@lab3.route('/lab3/form1/')
+def form1():
+    user = request.args.get('user')
+    age = request.args.get('age')
+    sex = request.args.get('sex')
+
+    if request.args.get('clear'):
+        resp = make_response(redirect(url_for('lab3.form1')))
+        resp.set_cookie('user', '', expires=0)
+        resp.set_cookie('age', '', expires=0)
+        resp.set_cookie('sex', '', expires=0)
+        return resp
+
+    if user or age or sex:
+        resp = make_response(render_template('lab3/form1.html', user=user, age=age, sex=sex))
+        if user:
+            resp.set_cookie('user', user)
+        if age:
+            resp.set_cookie('age', age)
+        if sex:
+            resp.set_cookie('sex', sex)
+        return resp
+
+    user = request.cookies.get('user')
+    age = request.cookies.get('age')
+    sex = request.cookies.get('sex')
+    return render_template('lab3/form1.html', user=user, age=age, sex=sex)
