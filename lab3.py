5eeac6d08642617dbe814e4750c0fae57378d4f9
GeorgyTimofeev <timofeevga22@gmail.com>
2024-10-16 09:51:32 +0700

    Очистка куки + билет

--- lab3.py
+++ lab3.py
@@ -123,6 +123,15 @@ def settings():
     font_weight = request.cookies.get('font_weight')
     return render_template('lab3/settings.html', color=color, bg_color=bg_color, font_size=font_size, font_weight=font_weight)
 
+@lab3.route('/lab3/clear_settings_cookies/', methods=['POST'])
+def clear_settings_cookies():
+    resp = make_response(redirect(url_for('lab3.settings')))
+    resp.delete_cookie('color')
+    resp.delete_cookie('bg_color')
+    resp.delete_cookie('font_size')
+    resp.delete_cookie('font_weight')
+    return resp
+
 @lab3.route('/lab3/form_railway/')
 def form_railway():
     return render_template('lab3/form_railway.html')
