ad58f7c14771f1169acced308378caf0f775f459
GeorgyTimofeev <timofeevga22@gmail.com>
2024-10-16 09:40:08 +0700

    Железнодорожный билет

--- lab3.py
+++ lab3.py
@@ -122,3 +122,47 @@ def settings():
     font_size = request.cookies.get('font_size')
     font_weight = request.cookies.get('font_weight')
     return render_template('lab3/settings.html', color=color, bg_color=bg_color, font_size=font_size, font_weight=font_weight)
+
+@lab3.route('/lab3/form_railway/')
+def form_railway():
+    return render_template('lab3/form_railway.html')
+
+@lab3.route('/lab3/ticket/', methods=['POST'])
+def ticket():
+    fio = request.form['fio']
+    berth = request.form['berth']
+    linen = 'linen' in request.form
+    baggage = 'baggage' in request.form
+    age = int(request.form['age'])
+    departure = request.form['departure']
+    destination = request.form['destination']
+    date = request.form['date']
+    insurance = 'insurance' in request.form
+
+    # Проверка возраста
+    if age < 1 or age > 120:
+        return "Возраст должен быть от 1 до 120 лет", 400
+
+    # Расчет стоимости билета
+    if age < 18:
+        ticket_type = "Детский билет"
+        price = 700
+    else:
+        ticket_type = "Взрослый билет"
+        price = 1000
+
+    if berth in ['нижняя', 'нижняя боковая']:
+        price += 100
+
+    if linen:
+        price += 75
+
+    if baggage:
+        price += 250
+
+    if insurance:
+        price += 150
+
+    return render_template('lab3/ticket.html', fio=fio, berth=berth, linen=linen, baggage=baggage, age=age,
+                           departure=departure, destination=destination, date=date, insurance=insurance,
+                           ticket_type=ticket_type, price=price)
