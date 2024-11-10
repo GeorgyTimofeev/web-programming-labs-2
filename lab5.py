5c07965245063ec6cc6087db27fffa022766217f
GeorgyTimofeev <timofeevga22@gmail.com>
2024-11-10 17:34:21 +0700

    Правки_6

--- lab5.py
+++ lab5.py
@@ -147,7 +147,7 @@ def create():
     if current_app.config['DB_TYPE'] == 'postgres':
         cur.execute("INSERT INTO articles (user_id, title, article_text) VALUES (%s, %s, %s)", (user_id, title, article_text))
     else:
-        cur.execute("INSERT INTO articles (user_id, title, article_text) VALUES (?, ?, ?)", (user_id, title, article_text))
+        cur.execute("INSERT INTO articles (login_id, title, article_text) VALUES (?, ?, ?)", (user_id, title, article_text))
 
     db_close(conn, cur)
     return redirect(url_for('lab5.lab'))
