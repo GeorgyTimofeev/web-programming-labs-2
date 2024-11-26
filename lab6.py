b8b69ec54df14586b810e6484646bf50b339e806
GeorgyTimofeev <timofeevga22@gmail.com>
2024-11-26 09:43:52 +0700

    Снятие аренды

--- lab6.py
+++ lab6.py
@@ -62,6 +62,44 @@ def api():
                     'id': id
                 }
 
+    if data['method'] == 'cancellation':
+        office_number = data['params']
+        if not login:
+            return {
+                'jsonrpc': '2.0',
+                'error': {
+                    'code': 1,
+                    'message': 'Unauthorized'
+                },
+                'id': id
+            }
+        office_number = data['params']
+        for office in offices:
+            if office['number'] == office_number:
+                if not office['tenant']:
+                    return {
+                        'jsonrpc': '2.0',
+                        'error': {
+                            'code': 3,
+                            'message': 'Office not booked'
+                        },
+                        'id': id
+                    }
+                if office['tenant'] != login:
+                    return {
+                        'jsonrpc': '2.0',
+                        'error': {
+                            'code': 4,
+                            'message': 'Cannot cancel booking of another user'
+                        },
+                        'id': id
+                    }
+                office['tenant'] = ""
+                return {
+                    'jsonrpc': '2.0',
+                    'result': 'Success',
+                    'id': id
+                }
     return {
         'jsonrpc': '2.0',
         'error': {
