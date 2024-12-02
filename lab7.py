c81306797935c72ed356287c39a5d5b47d8231a5
GeorgyTimofeev <timofeevga22@gmail.com>
2024-12-02 15:17:17 +0700

    Добавление нового фильма по REST

--- lab7.py
+++ lab7.py
@@ -90,3 +90,9 @@ def put_film(id):
     else:
         films[id] = film
         return films[id]
+
+@lab7.route('/lab7/rest-api/films/', methods=['POST'])
+def add_film():
+    new_film = request.get_json()
+    films.append(new_film)
+    return {'id': len(films) - 1}, 201
