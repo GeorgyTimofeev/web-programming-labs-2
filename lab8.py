202c4f4c9fcd4b020f846f10750675c018576a0d
GeorgyTimofeev <timofeevga22@gmail.com>
2024-12-12 00:19:25 +0700

    Регистрация

--- lab8.py
+++ lab8.py
@@ -4,7 +4,11 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 import sqlite3
 from os import path
+
+from werkzeug.security import generate_password_hash
 from lab5 import db_connect, db_close
+from db import db
+from db.models import users, articles, favorites, offices
 
 lab8 = Blueprint('lab8', __name__)
 
@@ -15,3 +19,26 @@ def inject_current_lab():
 @lab8.route('/lab8/')
 def lab():
     return render_template('lab8/lab8.html', login=session.get('login'))
+
+@lab8.route('/lab8/register/', methods = ['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('lab8/register.html')
+
+    login_form = request.form.get('login')
+    password_form = request.form.get('password')
+
+    if not login_form:
+        return render_template('lab8/register.html', error='Имя пользователя не должно быть пустым')
+    if not password_form:
+        return render_template('lab8/register.html', error='Пароль не должен быть пустым')
+
+    login_exists = users.query.filter_by(login=login_form).first()
+    if login_exists:
+        return render_template('lab8/register.html', error='Такой пользователь уже существует')
+
+    password_hash = generate_password_hash(password_form)
+    new_user = users(login=login_form, password=password_hash)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect('/lab8/')
